1b2f9a54a3980977d19eb376dc935e72ad5b324d
Katherine Otten Kim <kkatherine317@gmail.com>
2022-03-25 13:39:03 -0700

    add function body in function_d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,15 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    if len(numbers) == 1:
+        return numbers[0]
+        
+    max_num = numbers[0]
+    for num in numbers:
+        if num > max_num:
+            max_num = num
+    
+    return max_num
 
 
 if __name__ == "__main__":
